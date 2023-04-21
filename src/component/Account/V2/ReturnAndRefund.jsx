7f052b2e01a56ab505c2adcb86092d7fd242e55b
Vijay Singh <iamvijaysinghrajput@gmail.com>
2023-04-22 02:56:17 +0530

    okk

--- src/component/Account/V2/ReturnAndRefund.jsx
+++ src/component/Account/V2/ReturnAndRefund.jsx
@@ -27,180 +27,63 @@ const ReturnAndRefund = (props) => {
           with Us.
         </Text>
         <Text mb={2} fontSize={24} fontWeight={"100"}>
-          Interpretation and Definitions
+        The procedure for cancelling an order is as follows.
         </Text>
         <Box py={2}>
-          <Text fontSize={14} fontWeight={"600"} ml={2} mb={2}>
-            Interpretation
+          
+          <Text mb={2} fontSize={11}>
+          You have the option to cancel an order if it has already been placed or is in the process of being prepared.
           </Text>
           <Text mb={2} fontSize={11}>
-            The words of which the initial letter is capitalized have meanings
-            defined under the following conditions. The following definitions
-            shall have the same meaning regardless of whether they appear in
-            singular or in plural.
-          </Text>
-        </Box>
-        <Box py={2}>
-          <Text fontSize={14} fontWeight={"600"} ml={2} mb={2}>
-            Definitions
+          The order won't be cancelled if it has already been sent out for delivery.
           </Text>
           <Text mb={2} fontSize={11}>
-            For the purposes of this Return and Refund Policy:
+          If you cancel an order after it has been delivered to your door and the justification for the cancellation is false, you will be put on a blacklist. You won't ever have another opportunity to buy with us.
           </Text>
-          <UnorderedList>
-            <ListItem>
-              <Text fontWeight={"600"} as={"span"} mr={1}>
-                Application
-              </Text>
-              means the software program provided by the Company downloaded by
-              You on any electronic device, named SuperG.in
-            </ListItem>
-            <ListItem>
-              <Text fontWeight={"600"} as={"span"} mr={1}>
-                Company
-              </Text>
-              (referred to as either "the Company", "We", "Us" or "Our" in this
-              Agreement) refers to SuperG.in, 281 B Transport Nagar , Gorakhpur.
-            </ListItem>
-            <ListItem>
-              <Text fontWeight={"600"} as={"span"} mr={1}>
-                Goods
-              </Text>
-              refer to the items offered for sale on the Service.
-            </ListItem>
-            <ListItem>
-              <Text fontWeight={"600"} as={"span"} mr={1}>
-                Orders
-              </Text>
-              mean a request by You to purchase Goods from Us.
-            </ListItem>
-            <ListItem>
-              <Text fontWeight={"600"} as={"span"} mr={1}>
-                Service
-              </Text>
-              refers to the Application or the Website or both.
-            </ListItem>
-            <ListItem>
-              <Text fontWeight={"600"} as={"span"} mr={1}>
-                Website
-              </Text>
-              refers to SuperG.in, accessible from superg.in.
-            </ListItem>
-            <ListItem>
-              <Text fontWeight={"600"} as={"span"} mr={1}>
-                You
-              </Text>
-              means the individual accessing or using the Service, or the
-              company, or other legal entity on behalf of which such individual
-              is accessing or using the Service, as applicable.
-            </ListItem>
-          </UnorderedList>
         </Box>
+   
         <Text mb={2} fontSize={24} fontWeight={"100"}>
-          Your Order Cancellation Rights
+        Refund Policy
         </Text>
         <Text mb={2} fontSize={11}>
-          You are entitled to cancel Your Order within 1 days without giving any
-          reason for doing so.
+        We only offer replacements we don't offer refunds.
         </Text>
-        <Text mb={2} fontSize={11}>
-          The deadline for cancelling an Order is same day for vegetables ,
-          fruits, chiken and all fresh items from the date on which You received
-          the Goods or on which a third party you have appointed, who is not the
-          carrier, takes possession of the product delivered.
-        </Text>
-        <Text mb={2} fontSize={11}>
-          In order to exercise Your right of cancellation, You must inform Us of
-          your decision by means of a clear statement. You can inform us of your
-          decision by:
-          <UnorderedList mt={2}>
-            <ListItem>By email: support@superg.in</ListItem>
-            <ListItem>By phone number: 6391000414</ListItem>
-          </UnorderedList>
-        </Text>
-        <Text mb={2} fontSize={11}>
-          We will reimburse You no later than 14 days from the day on which We
-          receive the returned Goods. We will use the same means of payment as
-          You used for the Order, and You will not incur any fees for such
-          reimbursement.
-        </Text>
+       
         <Text mb={2} fontSize={24} fontWeight={"100"}>
           Conditions for Returns
         </Text>
         <Text mb={2} fontSize={11}>
-          In order for the Goods to be eligible for a return, please make sure
-          that:
+        Fresh products such as fruits, vegetables, poultry, milk, and curd can only be changed within six hours of the delivery of the order however, complaints regarding other grocery items can be made up to twenty-four hours in advance.
           <UnorderedList mt={2}>
             <ListItem>The Goods were purchased in the same day</ListItem>
             <ListItem>The Goods are in the original packaging</ListItem>
+            <ListItem> Vegetables - We only replace vegetables when they are not fresh in appearance from outside, or are damaged. After cutting the vegetable, if there is any problem inside, then we do not guarantee or replace it.</ListItem>
+            <ListItem> Fruits - We only replace Fruits when they are not fresh in appearance from outside, or are damaged. After cutting the Fruits, if there is any problem inside, then they do not guarantee or replace it.We cannot guarantee the sweetness of the fruits.</ListItem>
+            <ListItem> Poultry - We only replace poultry when they are not fresh in appearance from outside, or are damaged. After cutting the Poultry, if there is any problem inside, then they do not guarantee or replace it.</ListItem>
+            <ListItem> Fresh Items - We only replace Fresh Items when they are not fresh in appearance from outside, or are damaged. After cutting the Fresh Items, if there is any problem inside, then they do not guarantee or replace it.</ListItem>
           </UnorderedList>
         </Text>
         <Text mb={2} fontSize={11}>
-          The following Goods cannot be returned:
+        The replacement procedure is as follows.
           <UnorderedList mt={2}>
             <ListItem>
-              The supply of Goods made to Your specifications or clearly
-              personalized.
+            First, WhatsApp your order number to 6391000414 next, click on the image of the product you're unhappy with and last, WhatsApp 6391000414
             </ListItem>
             <ListItem>
-              The supply of Goods which according to their nature are not
-              suitable to be returned, deteriorate rapidly or where the date of
-              expiry is over.
+            If a legitimate reason for replacement is discovered after being examined by our quality team, that product will be replaced.
             </ListItem>
             <ListItem>
-              The supply of Goods which are not suitable for return due to
-              health protection or hygiene reasons and were unsealed after
-              delivery.
+            The next delivery window will be used to process order replacement.
             </ListItem>
-            <ListItem>
-              The supply of Goods which are, after delivery, according to their
-              nature, inseparably mixed with other items.
-            </ListItem>
+         
           </UnorderedList>
         </Text>
         <Text mb={2} fontSize={11}>
           We reserve the right to refuse returns of any merchandise that does
           not meet the above return conditions in our sole discretion.
         </Text>
-        <Text mb={2} fontSize={11}>
-          Only regular priced Goods may be refunded. Unfortunately, Goods on
-          sale cannot be refunded. This exclusion may not apply to You if it is
-          not permitted by applicable law.
-        </Text>
-        <Box>
-          <Text mb={2} fontSize={24} fontWeight={"100"}>
-            Returning Goods
-          </Text>
-          <Text mb={2} fontSize={11}>
-            You are responsible for the cost and risk of returning the Goods to
-            Us. You should send the Goods at the following address:
-          </Text>
-          <Text mb={2} fontSize={11}>
-            281 B Transport Nagar , Gorakhpur
-          </Text>
-          <Text mb={2} fontSize={11}>
-            We cannot be held responsible for Goods damaged or lost in return
-            shipment. Therefore, We recommend an insured and trackable mail
-            service. We are unable to issue a refund without actual receipt of
-            the Goods or proof of received return delivery.
-          </Text>
-        </Box>
-        <Box>
-          <Text mb={2} fontSize={24} fontWeight={"100"}>
-            Gifts
-          </Text>
-          <Text mb={2} fontSize={11}>
-            If the Goods were marked as a gift when purchased and then shipped
-            directly to you, You'll receive a gift credit for the value of your
-            return. Once the returned product is received, a gift certificate
-            will be mailed to You.
-          </Text>
-          <Text mb={2} fontSize={11}>
-            If the Goods weren't marked as a gift when purchased, or the gift
-            giver had the Order shipped to themselves to give it to You later,
-            We will send the refund to the gift giver.
-          </Text>
-        </Box>
+   
+
       </Box>
       <Box
         borderRadius={6}
